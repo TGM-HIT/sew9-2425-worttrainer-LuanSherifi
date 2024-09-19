4f74ae6d69de3fb5912b7bbe33f59676bc58dd01
Luan Sherifi <lsherifi@student.tgm.ac.at>
2024-09-19 12:21:11 +0200

    initial commit, setup new directory

---
+++ src/test/java/sherifi/worttrainer/WorttrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package sherifi.worttrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorttrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
