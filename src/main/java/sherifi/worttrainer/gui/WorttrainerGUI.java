c91dec712de390cd46099e235aeab7f5123337f8
luan <lsherifi@student.tgm.ac.at>
2024-10-16 12:17:27 +0200

    Bilder geupdated (Bug gefixed mit Placeholder Website)

--- src/main/java/sherifi/worttrainer/gui/WorttrainerGUI.java
+++ src/main/java/sherifi/worttrainer/gui/WorttrainerGUI.java
@@ -27,13 +27,12 @@ public class WorttrainerGUI {
             worttrainerService = new WorttrainerService();
             // Füge einige vordefinierte Wort-Bild-Paare hinzu
             try {
-                worttrainerService.addWortBildPaar(new WortBildPaar("Hund", "https://via.placeholder.com/150?text=Hund"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("Katze", "https://via.placeholder.com/150?text=Katze"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("Baum", "https://via.placeholder.com/150?text=Baum"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("Auto", "https://via.placeholder.com/150?text=Auto"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("Sherifi", "https://via.placeholder.com/150?text=Sherifi"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("SEW", "https://via.placeholder.com/150?text=SEW"));
-                worttrainerService.addWortBildPaar(new WortBildPaar("Test", "https://via.placeholder.com/150?text=Test"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("Hund", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQmEm06BSRd0EopLiNYprCoHc15FkT37RBQ&s"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("Katze", "https://media.os.fressnapf.com/cms/2021/05/katze_gefunde_zugelaufen_1200x527.jpg?t=seoimgsqr_527"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("Baum", "https://nikinclothing.com/cdn/shop/articles/der-baum-ein-meisterwerk-der-natur-356835.jpg?v=1713177674"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("Auto", "https://www.autoscout24.at/cms-content-assets/4mpSnBCf7cpBksE9w2nKmf-7d11ffa37ae4a3d95ae4823031e282f9-5kK7vUN4DMykm4fhsi0Pc-b1070949849a06dd22109fab7a99b8a2-lamborghini-huracan-tecnica-front-1100-1100.jpeg"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("SEW", "https://miro.medium.com/v2/resize:fit:1400/1*3OWWk9BUargTyvFGQpBsOA.png"));
+                worttrainerService.addWortBildPaar(new WortBildPaar("Test", "https://www.anti-bias.eu/wp-content/uploads/2015/01/shutterstock_92612287-e1420280083718.jpg"));
             } catch (Exception e) {
                 e.printStackTrace();
             }
